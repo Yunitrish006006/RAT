86d1527e0cfad6a3b616521fa998b5a6f68daf5a
Yunitrish <yunthomas006@gmail.com>
2023-01-05 23:25:26 +0800

    update

--- components.py
+++ components.py
@@ -1,14 +1,58 @@
 import discord
 from discord.ui import Select
 from discord import SelectOption
-from Log import log
-
+from discord.utils import get
+from discord import Colour
+#================================================================
+from datetime import datetime
+class log:
+    text=""
+    time_formet="[%Y/%m/%d %H:%M:%S]  "
+    sync_log = False
+    def __init__(self,time_mode:str = "default",sync_log:bool=False) -> None:
+        self.set_time_mode(time_mode)
+        self.text+="⚙️[ Log System ]\n"
+        self.sync_log=sync_log
+    def println(self,content:str="\n"):
+        temp=datetime.now().strftime(self.time_formet)+" "+content
+        self.text+=temp+"\n"
+        if self.sync_log:print(temp)
+    def print(self,content:str="\n"):
+        temp=datetime.now().strftime(self.time_formet)+" "+content
+        self.text+=temp
+        if self.sync_log:print(temp)
+    def show(self):
+        return self.text
+    def set_time_mode(self,mode:str):
+        if(mode=="off"):
+            self.time_formet=""
+        elif(mode=="default"):
+            self.time_formet="[%Y/%m/%d %H:%M:%S]"
+        elif(mode=="time_only"):
+            self.time_formet="[%H:%M:%S]"
+        elif(mode=="day_only"):
+            self.time_formet="[%Y/%m/%d]"
+#statistic=======================================================
+available_servers = ['彰化師範大學','無名氏','三隻狗','Yun 的伺服器',"Yunitrish's server","嵌入式系統 測試伺服器"]
+#functions=======================================================
+def pickup(guild_list:list[discord.Guild],log:log):
+    to_enable:list[discord.Guild] = []
+    for g in guild_list:
+            if g.name in available_servers:
+                to_enable.append(g)
+                log.println("🟢  " + g.name)
+            else:
+                log.println("🔴  " + g.name)
+    return to_enable
+#================================================================
 jobs = [
     discord.SelectOption(label="miner",emoji="⛏️",description="a career that can get lot of jeweris"),
     discord.SelectOption(label="farmer",emoji="🥦",description="a career that can grow foods"),
     discord.SelectOption(label="smith",emoji="⚒️",description="a career that can make tools and weapons"),
     discord.SelectOption(label="hunter",emoji="🏹",description="a career that can get meats from nature")
 ]
+job_colors = [Colour.dark_gold(),Colour.green(),Colour.dark_grey(),Colour.from_rgb(139,69,19)]
+#=====================================================================彰師大伺服器專用========================================================================
 departments = [
     discord.SelectOption(label="資工系",emoji="👓",description="資訊工程學系"),
     discord.SelectOption(label="資管系",emoji="🔐",description="資訊管理學系"),
@@ -16,25 +60,3 @@ departments = [
     discord.SelectOption(label="機電系",emoji="⚙️",description="機電工程學系")
 ]
 grades = [discord.SelectOption(label=str(i)) for i in range(109,115)]
-
-available_servers = ['彰化師範大學','無名氏','三隻狗','Yun 的伺服器']
-
-class slection(Select):
-    def __init__(self, *, custom_id: str = "job_selection", placeholder:str = "select your job", min_values: int = 1, max_values: int = 1, options: list[SelectOption] = ..., disabled: bool = False, row: int = None) -> None:
-        super().__init__(custom_id=custom_id, placeholder=placeholder, min_values=min_values, max_values=max_values,options=options, disabled=disabled, row=row)
-
-
-def pickup(guild_list:list,log:log):
-    temp = []
-    for g in guild_list:
-            if g.name in available_servers:
-                temp.append(g)
-                log.println("🟢  " + g.name)
-            else:
-                log.println("🔴  " + g.name)
-    return temp
-def get_ids(guild_list:list):
-    temp = []
-    for guild in guild_list:
-        temp.append(guild.id)
-    return temp
--- rat.py
+++ rat.py
@@ -1,68 +1,62 @@
+from typing import Optional
 import discord
+import discord.ui
 from discord import app_commands
-from discord.ui import Button, View, Select
+from components import log,pickup
 from discord.utils import get
-from discord import Colour
-from Log import log
-from components import available_servers,pickup,jobs,departments,grades,get_ids
-NCUE = 764126539041734679
-token="MTAyMDM1OTg0NTU5NTA1NDA4MA.G9cosw.6xEknoV9nayUjHJFr8vE8_a-UgLOASGya2TZfk"
-guild_list:list[discord.Object] = []
+from components import jobs,job_colors
+from random import randint
+
+ENABLED_GUILDS:list[discord.Object]=[]
+
+GUILDS:list[discord.Guild]=[]
 
 
-GIDS = []
+class RAT(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+    async def setup_hook(self):
+        # for guild in [discord.Object(id=1060524409460379768),discord.Object(id=764126539041734679),discord.Object(id=1003642488826900551)]:
+        #     self.tree.copy_global_to(guild=guild)
+        #     await self.tree.sync(guild=guild)
+        print("setting up hook")
 
-async def initialize_roles(guild_id,selections:list[discord.SelectOption],colors:list[discord.Colour]):
-    guild = client.get_guild(guild_id)
-    for n in range(len(selections)):
-        if selections[n].emoji == None : val = selections[n].label
-        else: val = (selections[n].emoji).name+"."+selections[n].label
-        if not get(guild.roles,name=val): 
-            await guild.create_role(name=val,hoist=True,colour=colors[n])
-history = log()
-class bot_client(discord.Client):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.synced = False
-        global guild_obj_list
-        guild_obj_list = [
-            discord.Object(id=1003642488826900551),
-            discord.Object(id=1058639961500426320),
-            discord.Guild
-        ]
-    async def on_ready(self):
-        global history,guild_list,guild_obj_list2,GIDS
-        guild_obj_list2 = [str(i.name)+" , "+str(i.id) for i in client.guilds]
-        await self.wait_until_ready()
-        guild_list = [g for g in client.guilds]
-        guild_list = pickup(guild_list,history)
-        for i in guild_list: GIDS.append(i.id)
-        
-        if not self.synced:
-            for guild in guild_list:
-                history.println("Synchronizing "+guild.name+".....")
-                await tree.sync(guild=discord.Object(id=guild.id))
-            self.synced = True
-        for i in guild_list:
-            career_colors = [Colour.dark_gold(),Colour.green(),Colour.dark_grey(),Colour.from_rgb(139,69,19)]
-            if(i.id == NCUE):
-                await initialize_roles(guild_id=i.id,selections=departments+grades,colors=[Colour.blue() for _ in range(len(departments+grades))])
-            else:
-                await initialize_roles(guild_id=i.id,selections=jobs,colors=career_colors)
-        
-        history.println("GUILDS:")
-        for i in guild_list:
-            history.println(i.name)
-        history.println("logged in as " + self.user.display_name)
-        print(history.show())
+client = RAT(intents=discord.Intents.default())
+history = log(sync_log=True)
 
-client = bot_client()
-tree = app_commands.CommandTree(client)
+@client.event
+async def on_ready():
+    history.println(f'~ $ 機器人[{client.user}](ID: {client.user.id})已啟動')
+    global GUILDS,ENABLED_GUILDS
+    GUILDS = [guild for guild in client.guilds]
+    history.println('=====================篩選伺服器中=======================')
+    GUILDS = pickup(GUILDS,history)
+    history.println("======================篩選後名單========================")
+    for guild in GUILDS:
+        history.println(guild.name)
+        ENABLED_GUILDS.append(discord.Object(id=guild.id))
+    history.println('===================同步伺服器指令中=====================')
+    for guild in ENABLED_GUILDS:
+        history.println("同步"+client.get_guild(guild.id).name+"指令中....")
+        client.tree.copy_global_to(guild=guild)
+        await client.tree.sync(guild=guild)
+    history.println("=====================初始化身分組=======================")
+    for go in ENABLED_GUILDS:
+        guild = client.get_guild(go.id)
+        for selection,color in zip(jobs,job_colors):
+            if selection.emoji == None : role_name = selection.label
+            else: role_name = (selection.emoji).name+"."+selection.label
+            if not get(guild.roles,name=role_name): 
+                await guild.create_role(name=role_name,hoist=True,colour=color)
+                history.println("創建 "+role_name+" 至 "+guild.name)
+        history.println(guild.name+" 初始化完畢")
+    history.println("========================================================")
 
-class job_select(Select):
-    def __init__(self):
+class job_select(discord.ui.Select):
+    def __init__(self,history:log):
         super().__init__(
-            placeholder ="select your job",
+            placeholder ="職業類別",
             min_values=1,
             max_values=1,
             options=jobs
@@ -70,88 +64,105 @@ class job_select(Select):
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user
         guild = interaction.guild
-        await interaction.response.send_message(f"{user.name} select {self.values[0]} as job",ephemeral=True)
+        ########################################################################
+        def add_userdata(add):
+            out = []
+            lst = [line.strip('\n').split(" ") for line in open('database/jobs.txt').readlines()]
+            for i in lst:
+                cnt = 0
+                #把檔案重複名字更新只留最後一個
+                for j in out:
+                    if i[0]==j[0]:
+                        j[1]=i[1]
+                    else:
+                        cnt+=1
+                if cnt == len(out):
+                    out.append(i)
+            #如果新加入的user名字存在在檔案中 -> 更新檔案
+            cnt = 0
+            for i in out:
+                if add[0]==i[0]:
+                    i[1]=add[1]
+                else:
+                    cnt += 1
+            if cnt == len(out):
+                out.append(add)
+            with open('database/jobs.txt', 'w') as f:
+                for i in out:
+                    f.write(str(i[0])+" "+str(i[1])+"\n")
+        ########################################################################
+        user_name=str(user.name).replace(" ","_")
+        add_userdata([user_name,str(self.values[0])])
+        await interaction.response.send_message(f"{user_name} 選擇了 {self.values[0]} 作為職業",ephemeral=True)
+        history.println(f"{user_name} 選擇了 {self.values[0]} 作為職業")
         for i in self.options:
             current =  get(guild.roles, name=i.emoji.name+"."+i.label)
             if current.name.split(".")[1] == self.values[0]: await user.add_roles(current)
             else:  await user.remove_roles(current)
 
-class department_select(Select):
-    def __init__(self):
-        super().__init__(
-            placeholder ="選擇你的科系",
-            min_values=1,
-            max_values=1,
-            options=departments
-        )
-    async def callback(self, interaction: discord.Interaction):
-        user = interaction.user
-        guild = interaction.guild
-        await interaction.response.send_message(f"{user.name} 已加入 {self.values[0]}",ephemeral=True)
-        for i in self.options:
-            current =  get(guild.roles, name=i.emoji.name+"."+i.label)
-            if current.name.split(".")[1] == self.values[0]: await user.add_roles(current)
-            else:  await user.remove_roles(current)
+@client.tree.command(name="選擇職業",description="選取職業")
+async def user_work(interaction: discord.Interaction):
+    user_name = (interaction.user.name).replace(" ","_")
+    await interaction.response.send_message(f"{user_name},選擇你的職業: ",view=discord.ui.View().add_item(job_select(history=history)),ephemeral=True)
 
-class grade_select(Select):
-    def __init__(self):
-        super().__init__(
-            placeholder = "選擇你的級數",
-            min_values=1,
-            max_values=1,
-            options=grades
-        )
-    async def callback(self, interaction: discord.Interaction):
-        user = interaction.user
-        guild = interaction.guild
-        await interaction.response.send_message(f"{user.name} 已加入 {self.values[0]}",ephemeral=True)
-        for i in self.options:
-            current =  get(guild.roles, name=i.label)
-            if current.name==self.values[0]: await user.add_roles(current)
-            else:  await user.remove_roles(current)
+@client.tree.command(name="工作",description="賺錢")
+async def user_work(interaction: discord.Interaction):
+    earned = str(randint(1,100))
+    user_name = (interaction.user.name).replace(" ","_")
+    with open("database/works.txt",'a') as work_file:
+        work_file.write(f"earned {user_name} "+earned+"\n")
+    await interaction.response.send_message(f"{user_name} 賺到了 "+earned+" 元\n",ephemeral=True)
 
-class yes_no_view(View):
-    def __init__(self, *, timeout = 180):
-        super().__init__(timeout=timeout)
-        self.add_item(Button(style=discord.ButtonStyle.green,emoji="✔"))
-        self.add_item(Button(style=discord.ButtonStyle.red,emoji="✘"))
+@client.tree.command(name="查詢機器人服務伺服器")
+async def get_bot_served(interaction: discord.Interaction):
+    """查詢機器人服務伺服器"""
+    reply = client.user.mention + "服務於:\n"
+    for guild in GUILDS:
+        reply += guild.name + "\n"
+    await interaction.response.send_message(reply,ephemeral=True)
 
-class select_view(View):
-    def __init__(self,item,timeout=180):
-        super().__init__(timeout=timeout)
-        self.add_item(item)
+@client.tree.command(name="查詢機器人執行紀錄")
+async def get_bot_log(interaction: discord.Interaction):
+    """執行紀錄查詢"""
+    await interaction.response.send_message(history.show(),ephemeral=True)
 
-@tree.command(name="查詢機器人服務伺服器",description="查詢機器所在的服務器",guilds=guild_obj_list)
-async def self(i:discord.Interaction):
-    history.println("<"+i.user.name+"> send a command "+self.name)
-    guilds = i.client.guilds
-    len(guilds)
-    show = "⚙️\n"
-    for guild in guilds: show+="["+str(guild.id)+"] "+guild.name+"\n"
-    await i.response.send_message(f"{show}")
-    print(show)
-
-@tree.command(name="查詢機器人紀錄",description="顯示此次開機以後的所有過程",guilds=guild_obj_list)
-async def self(i:discord.Interaction):
-    history.println("<"+i.user.name+"> send a command "+self.name)
-    await i.response.send_message(f"{history.show()}")
-
-@tree.command(name="close",description="turn off this bot globally",guilds=guild_obj_list)
-async def self(interaction:discord.Interaction,password:str):
-    if password=="ratoff":
-        await interaction.response.send_message(f"turing off rat .....")
+@client.tree.command(name="關閉機器人")
+@app_commands.describe(password='機器人金鑰')
+@app_commands.rename(password='金鑰')#在使用者看到的地方將變數改為該字串
+async def close_bot(interaction: discord.Interaction,password: str):
+    """關閉機器人"""
+    if(password=="ratoff"):
+        await interaction.response.send_message(f'{interaction.user.name} 關閉了機器人')
         await client.close()
+    else:
+        await interaction.response.send_message(f'{interaction.user.name} 正在嘗試關閉機器人!')
 
-@tree.command(name="job",description="test of selector",guilds=guild_obj_list)
-async def self(interaction:discord.Interaction):
-    await interaction.response.send_message(f"select your job",view=select_view(job_select()),ephemeral=True)
+@client.tree.command(name="取得成員加入時間")
+@app_commands.describe(member='取得成員加入的時間')
+async def get_member_join_time(interaction: discord.Interaction, member: Optional[discord.Member] = None):#Optional示範
+    """Says when a member joined."""
+    member = member or interaction.user
+    await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
 
-@tree.command(name="選擇科系",description="選擇自己科系以加入相關討論區",guilds=guild_obj_list)
-async def self(interaction:discord.Interaction):
-    await interaction.response.send_message(f"選擇你的科系",view=select_view(department_select()),ephemeral=False)
 
-@tree.command(name="選擇級數",description="選擇自己級數以加入相關討論區",guilds=guild_obj_list)
-async def self(interaction:discord.Interaction):
-    await interaction.response.send_message(f"選擇你的級數",view=select_view(grade_select()),ephemeral=False)
+#在context_menu中，第二個選項為被作用對象
 
-client.run(token)
+@client.tree.context_menu(name='取得成員加入時間')
+async def show_member_join_date(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}',ephemeral=True)
+
+@client.tree.context_menu(name='舉報給管理者')
+async def report_message(interaction: discord.Interaction, message: discord.Message):
+    await interaction.response.send_message( f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True)
+    log_channel = interaction.guild.get_channel(interaction.channel_id)  # replace with your channel id
+    embed = discord.Embed(title='被舉報訊息')
+    if message.content:
+        embed.description = message.content
+    embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+    embed.timestamp = message.created_at
+    url_view = discord.ui.View()
+    url_view.add_item(discord.ui.Button(label='前往該訊息', style=discord.ButtonStyle.url, url=message.jump_url))
+    await log_channel.send(embed=embed, view=url_view)
+
+
+client.run("MTAyMDM1OTg0NTU5NTA1NDA4MA.G-Vq1n.KPJwmcg3IXAooZLoLRwy_sIq2YtdVLAl_L1cKY")
